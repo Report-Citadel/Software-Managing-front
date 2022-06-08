0a6192a78aa98aa4abf1c5b3e5581ae39be3f582
XZQ <1442391739@qq.com>
2022-06-08 18:37:20 +0800

    一点修改

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -279,40 +279,41 @@ export default {
           baseURL: "/api",
           url: "/user/login",
 
-          data: this.ruleForm
-        }).then((res) => {
-          if (res.data.code === 200) {
-            sessionStorage.setItem("role", 1);
-            this.$message({
-              message: "登陆成功",
-              type: "success",
-            });
-            // console.log(res);
-            sessionStorage.setItem("role", res.data.data.identity);
-            sessionStorage.setItem("authorities", res.data.data.authorities);
-            sessionStorage.setItem("token", res.data.data.token);
-            sessionStorage.setItem("id", res.data.data.userid);
-            console.log(res.data.data.identity === 1)
-            switch (res.data.data.identity) {
-              case "1":
-                console.log("管理员")
-                this.$router.push("/adminHome");
-                break;
-              case "2":
-                this.$router.push("/teacherHome/control");
-                break;
-              case "3":
-                this.$router.push("/teacherHome/control");
-                break;
-              case "4":
-                this.$router.push("/studentHome/control");
-                break;
-              case "5":
-                this.$router.push("/studentHome/control");
-                break;
-              default:
-                break;
-
+          data: this.ruleForm,
+        })
+          .then((res) => {
+            if (res.data.code === 200) {
+              sessionStorage.setItem("role", 1);
+              this.$message({
+                message: "登陆成功",
+                type: "success",
+              });
+              // console.log(res);
+              sessionStorage.setItem("role", res.data.data.identity);
+              sessionStorage.setItem("authorities", res.data.data.authorities);
+              sessionStorage.setItem("token", res.data.data.token);
+              sessionStorage.setItem("id", res.data.data.userid);
+              console.log(res.data.data.identity === 1);
+              switch (res.data.data.identity) {
+                case "1":
+                  console.log("管理员");
+                  this.$router.push("/adminHome");
+                  break;
+                case "2":
+                  this.$router.push("/teacherHome/control");
+                  break;
+                case "3":
+                  this.$router.push("/teacherHome/control");
+                  break;
+                case "4":
+                  this.$router.push("/studentHome/control");
+                  break;
+                case "5":
+                  this.$router.push("/studentHome/control");
+                  break;
+                default:
+                  break;
+              }
             }
           })
           .catch((err) => {
